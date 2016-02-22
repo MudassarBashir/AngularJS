5234ce87539c911be289f1b6cf92258200f3ebf4
U-LOGIXML\mbashir <mbashir@LGXMCDEV00781.LogiXML.local>
2016-02-22 16:04:16 -0500

    Using the XMLHTTPRequest object.

--- app.js
+++ app.js
@@ -1,12 +1,30 @@
 var myApp = angular.module('myApp', []);
 
 
-myApp.controller('mainController', ['$scope', function($scope) {
+myApp.controller('mainController', ['$scope', '$filter', function($scope, $filter, $timeout) {
     
-    $scope.alertClick = function () {
-        alert("Clicked!");
+    $scope.handle = '';
+    
+    $scope.lowerCaseHandle = function () {
+      return $filter('lowercase')($scope.handle);  
     };
     
-    $scope.name = 'John Doe';
+    $scope.characters = 5;
+    
+    var rulesRequest = new XMLHttpRequest();
+    
+    rulesRequest.onreadystatechange = function () {
+        
+        $scope.$apply(function () {
+            if (rulesRequest.readyState == 4 && rulesRequest.status == 200) {
+                $scope.rules = JSON.parse(rulesRequest.responseText); 
+            }
+        });
+    }
+    
+    rulesRequest.open("GET", "http://localhost:8080/rest/index",
+                     true);
+    rulesRequest.send();
+    
     
 }]);
