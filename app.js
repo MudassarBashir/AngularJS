65098bc74bfc2f06bd32e4da8ca7b17bd0f1ad79
U-LOGIXML\mbashir <mbashir@LGXMCDEV00781.LogiXML.local>
2016-02-22 16:54:13 -0500

    External data and http.

--- app.js
+++ app.js
@@ -18,4 +18,16 @@ myApp.controller('mainController', ['$scope', '$filter', '$http', function($scop
         .error(function(data, status) {
             console.log(data);
         });
+    
+    $scope.newRule = '';
+    $scope.addRule = function () {
+        $http.post('http://localhost:8080/rest/index', {newRule: $scope.newRule})
+            .success(function(result) {
+                $scope.rules = result;
+                $scope.newRule = '';
+            })
+            .error(function(data) {
+                console.log(data);
+            });
+    };
 }]);
