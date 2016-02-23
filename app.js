9da104c8b32d4fb993fdc03c676c2a11b9e26524
U-LOGIXML\mbashir <mbashir@LGXMCDEV00781.LogiXML.local>
2016-02-23 15:54:26 -0500

    Multiple controllers, multiple views.

--- app.js
+++ app.js
@@ -1,33 +1,14 @@
 var myApp = angular.module('myApp', []);
 
 
-myApp.controller('mainController', ['$scope', '$filter', '$http', function($scope, $filter, $http) {
+myApp.controller('mainController', ['$scope', function($scope) {
     
-    $scope.handle = '';
+    $scope.name = 'Main';
     
-    $scope.lowerCaseHandle = function () {
-      return $filter('lowercase')($scope.handle);  
-    };
+}]);
+
+myApp.controller('secondController', ['$scope', function($scope) {
     
-    $scope.characters = 5;
+    $scope.name = 'Second';
     
-    $http.get('http://localhost:8080/rest/index')
-        .success(function(result) {
-           $scope.rules = result; 
-        })
-        .error(function(data, status) {
-            console.log(data);
-        });
-    
-    $scope.newRule = '';
-    $scope.addRule = function () {
-        $http.post('http://localhost:8080/rest/index', {newRule: $scope.newRule})
-            .success(function(result) {
-                $scope.rules = result;
-                $scope.newRule = '';
-            })
-            .error(function(data) {
-                console.log(data);
-            });
-    };
 }]);
