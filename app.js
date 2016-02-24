76fb5fb721b325a62e242810eaf498d7de7fa77c
U-LOGIXML\mbashir <mbashir@LGXMCDEV00781.LogiXML.local>
2016-02-24 17:23:28 -0500

    Logging the hash's value to the console.

--- app.js
+++ app.js
@@ -1,15 +1,7 @@
-window.addEventListener('hashchange', function() {
+var myApp = angular.module('myApp', []);
+
+
+myApp.controller('mainController', ['$scope', '$location', '$log', function($scope, $location, $log) {
     
-    if (window.location.hash === '#/bookmark/1') {
-        console.log('Page 1 is cool.');
-    }
-    
-    if (window.location.hash === '#/bookmark/2') {
-        console.log('Page 2 is cool.');
-    }
-    
-    if (window.location.hash === '#/bookmark/3') {
-        console.log('Page 3 is cool.');
-    }
-    
-});
+    $log.info($location.path());
+}]);
