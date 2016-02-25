8f20621d1dccc19320db9859c46366d12158f399
Moe B <moe.bashir@logianalytics.com>
2016-02-25 14:53:42 -0500

    Routing, Templates, and Controllers.

--- app.js
+++ app.js
@@ -1,7 +1,29 @@
-var myApp = angular.module('myApp', []);
+var myApp = angular.module('myApp', ['ngRoute']);
+
+myApp.config(function($routeProvider) {
+
+    $routeProvider
+
+        .when('/', {
+            templateUrl: 'pages/main.html',
+            controller: 'mainController'
+        })
+
+        .when('/second', {
+            templateUrl: 'pages/second.html',
+            controller: 'secondController'
+        })
+});
 
 
-myApp.controller('mainController', ['$scope', '$location', '$log', function($scope, $location, $log) {
-    
-    $log.info($location.path());
+myApp.controller('mainController', ['$scope', '$log', function($scope, $log) {
+
+    $scope.name = 'Main';
+
+}]);
+
+myApp.controller('secondController', ['$scope', '$log', function($scope, $log) {
+
+    $scope.name = 'Second';
+
 }]);
