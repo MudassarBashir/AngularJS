39d6f0998ee90b13c92a4fd8e14317176dfc077a
Moe B <moe.bashir@logianalytics.com>
2016-03-03 12:16:21 -0500

    An AngularJS weather SPA(single page application). Initial commit.

--- app.js
+++ app.js
@@ -1,85 +1,31 @@
-var myApp = angular.module('myApp', ['ngRoute']);
+// Module
+var weatherApp = angular.module('weatherApp', ['ngRoute', 'ngResource']);
 
-myApp.config(function($routeProvider) {
+// Routes
+weatherApp.config(function ($routeProvider) {
 
     $routeProvider
 
         .when('/', {
-            templateUrl: 'pages/main.html',
-            controller: 'mainController'
+            templateUrl: 'pages/home.html',
+            controller: 'homeController'
         })
 
-        .when('/second', {
-            templateUrl: 'pages/second.html',
-            controller: 'secondController'
-        })
-
-        .when('/second/:num', {
-            templateUrl: 'pages/second.html',
-            controller: 'secondController'
+        .when('/forecast', {
+            templateUrl: 'pages/forecast.html',
+            controller: 'forecastController'
         })
 });
 
-myApp.service('nameService', function() {
+// Controllers
+weatherApp.controller('homeController', ['$scope', function($scope) {
 
-    var self = this;
-    this.name = 'John Doe';
-
-    this.namelength = function() {
-      return self.name.length;
-    };
-
-});
-
-myApp.controller('mainController', ['$scope', '$log', 'nameService', function($scope, $log, nameService) {
-
-    $scope.people = [
-        {
-            name: "John Doe",
-            address: '555 Main. St.',
-            city: 'New York',
-            state: 'NY',
-            zip: '11111'
-        },
-        {
-            name: "Jane Doe",
-            address: '333 Second St.',
-            city: 'Las Vegas',
-            state: 'NV',
-            zip: '77777'
-        },
-        {
-            name: "George Doe",
-            address: '6782 Cuba St.',
-            city: 'Miami',
-            state: 'FL',
-            zip: '88888'
-        }
-    ]
-
-    $scope.formattedAddress = function(person) {
-
-        return person.address + ', ' + person.city + ', ' + person.state + ', ' + person.zip;
-
-    };
-
-}]);
-
-myApp.controller('secondController', ['$scope', '$log', '$routeParams', 'nameService', function($scope, $log, $routeParams, nameService) {
 
 
 }]);
 
-myApp.directive("searchResult", function () {
+weatherApp.controller('forecastController', ['$scope', function($scope) {
 
-    return {
-        restrict: 'AECM',
-        templateUrl: 'directives/searchresult.html',
-        replace: true,
-        scope: {
-            personObject: "=",
-            formattedAddressFunction: "&"
-        },
-        transclude: true
-    }
-});
+
+
+}]);
