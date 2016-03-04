5e8df03efa478e57755cc092453a70d2d9db4c2f
Moe B <moe.bashir@logianalytics.com>
2016-03-04 12:28:43 -0500

    AngularJS weather SPA: Data binding part 1.

--- app.js
+++ app.js
@@ -15,12 +15,17 @@ weatherApp.config(function ($routeProvider) {
             templateUrl: 'pages/forecast.html',
             controller: 'forecastController'
         })
+
+        .when('/forecast/:days', {
+            templateUrl: 'pages/forecast.html',
+            controller: 'forecastController'
+        })
 });
 
 // Services
 weatherApp.service('cityService', function() {
 
-    this.city = "New York, NY";
+    this.city = "Miami, FL";
 
 });
 
@@ -34,8 +39,32 @@ weatherApp.controller('homeController', ['$scope', 'cityService', function($scop
 
 }]);
 
-weatherApp.controller('forecastController', ['$scope', 'cityService', function($scope, cityService) {
+weatherApp.controller('forecastController', ['$scope', '$resource', '$routeParams', 'cityService', function($scope, $resource, $routeParams, cityService) {
 
     $scope.city = cityService.city;
+    $scope.days = $routeParams.days || 2;
+
+    $scope.weatherAPI =
+            $resource("http://api.openweathermap.org/data/2.5/forecast/daily?APPID=8c6cbd6fb58035ffffaf257966bc6314",
+            { callback: "JSON_CALLBACK"},
+            { get: { method: "JSONP"}});
+    $scope.weatherResult = $scope.weatherAPI.get({q: $scope.city, cnt: $scope.days});
+
+    $scope.convertToFahrenheit = function (kelvin) {
+
+        return Math.round((1.8 * (kelvin - 273)) + 32);
+
+    }
+
+    $scope.convertToPrettyDate = function(uglyDate) {
+
+        return new Date(uglyDate * 1000);
+
+    }
 
 }]);
+
+// api key
+// 8c6cbd6fb58035ffffaf257966bc6314
+/* Example forcast for Miami 7 days. http://api.openweathermap.org/data/2.5/forecast/daily?q=Miami&cnt=7&APPID=8c6cbd6fb58035ffffaf257966bc6314
+*/
