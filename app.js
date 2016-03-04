8f3f24e6cdbb1abdbea28beba2bb7850144f0569
Moe B <moe.bashir@logianalytics.com>
2016-03-04 14:03:41 -0500

    AngularJS weather SPA: Data binding part 2.

--- app.js
+++ app.js
@@ -42,13 +42,15 @@ weatherApp.controller('homeController', ['$scope', 'cityService', function($scop
 weatherApp.controller('forecastController', ['$scope', '$resource', '$routeParams', 'cityService', function($scope, $resource, $routeParams, cityService) {
 
     $scope.city = cityService.city;
-    $scope.days = $routeParams.days || 2;
+    $scope.days = $routeParams.days || '2';
+    // I had to sign up for this free API key.
+    $scope.apiKey = "8c6cbd6fb58035ffffaf257966bc6314";
 
     $scope.weatherAPI =
-            $resource("http://api.openweathermap.org/data/2.5/forecast/daily?APPID=8c6cbd6fb58035ffffaf257966bc6314",
+            $resource("http://api.openweathermap.org/data/2.5/forecast/daily?",
             { callback: "JSON_CALLBACK"},
             { get: { method: "JSONP"}});
-    $scope.weatherResult = $scope.weatherAPI.get({q: $scope.city, cnt: $scope.days});
+    $scope.weatherResult = $scope.weatherAPI.get({q: $scope.city, cnt: $scope.days, APPID: $scope.apiKey});
 
     $scope.convertToFahrenheit = function (kelvin) {
 
