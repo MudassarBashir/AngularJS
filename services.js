6aa0803ec62cd787c6112738ffdb5301587c70c4
Moe B <moe.bashir@logianalytics.com>
2016-03-09 11:18:40 -0500

    Improving our SPA:  Moving work performed to service and keeping controllers lean(best practice).

--- services.js
+++ services.js
@@ -6,3 +6,13 @@ weatherApp.service('cityService', function() {
 
 });
 
+weatherApp.service('weatherService', ['$resource', function($resource) {
+
+    this.GetWeather = function (city, days, apiKey) {
+        var weatherAPI =
+            $resource("http://api.openweathermap.org/data/2.5/forecast/daily?",
+            { callback: "JSON_CALLBACK"},
+            { get: { method: "JSONP"}});
+        return weatherAPI.get({q: city, cnt: days, APPID: apiKey});
+    }
+}]);
